9347fa8df5605cb79a24e8abee908e306fe29d5a
khalilmalak1022 <khalilmalak1022@gmail.com>
2026-04-01 10:20:53 +0100

    Phase 2 - models.py et admin.py terminés

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profil, Categorie, Transaction, Objectif, Budget, Notification
 
-# Register your models here.
+admin.site.register(Profil)
+admin.site.register(Categorie)
+admin.site.register(Transaction)
+admin.site.register(Objectif)
+admin.site.register(Budget)
+admin.site.register(Notification)
