09f7ce4eef0aa6112afb566060a6c09228da39aa
Samalile <samalile2703@gmail.com>
2026-05-09 14:55:16 +0100

    changement d'url racine

--- budget_app/urls.py
+++ budget_app/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', lambda request: redirect('login')),
+
+    # Auth
     path('', include('apps.accounts.urls')),
-    path('', include('apps.transactions.urls')),
-    path('', include('apps.dashboard.urls')),
-    path('', include('apps.objectifs.urls')),
-    path('', include('apps.budgets.urls')),
-    path('', include('apps.notifications.urls')),
-    path('', include('apps.export.urls')),
-    path('', include('apps.chatbot.urls')),
+
+    # Modules principaux
+    path('transactions/', include('apps.transactions.urls')),
+    path('dashboard/', include('apps.dashboard.urls')),
+    path('objectifs/', include('apps.objectifs.urls')),
+    path('budgets/', include('apps.budgets.urls')),
+    path('notifications/', include('apps.notifications.urls')),
+    path('export/', include('apps.export.urls')),
+    path('chatbot/', include('apps.chatbot.urls')),
 ]
